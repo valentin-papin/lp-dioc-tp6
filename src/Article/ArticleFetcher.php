663e1361b860e9921391cf8edd7124c90fb0ea68
valentin-papin <valentin.papin1@gmail.com>
2017-11-29 11:12:53 +0100

    Ajout de l'ArticleFetcher

--- src/Article/ArticleFetcher.php
+++ src/Article/ArticleFetcher.php
@@ -4,14 +4,22 @@ namespace App\Article;
 
 use App\Entity\Article;
 use Doctrine\Bundle\DoctrineBundle\Registry;
+use Doctrine\ORM\EntityManager;
 
 class ArticleFetcher
 {
+    private $em;
+
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+    }
+
     public function fetch() : array
     {
         // Retourne les 10 derniers articles.
         // La limit (ici 10) doit provenir d'une variable d'env.
 
-        return [];
+        return $this->em->getRepository(Article::class)->findAll();
     }
 }
--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -12,6 +12,12 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use App\Slug;
+use Symfony\Component\Validator\Constraints\Date;
+use Symfony\Component\VarDumper\Cloner\Data;
+use DateTime;
 
 /**
  * @Route(path="/article")
@@ -21,22 +27,42 @@ class ArticleController extends Controller
     /**
      * @Route(path="/show/{slug}", name="article_show")
      */
-    public function showAction()
+    public function showAction(ViewArticleHandler $articleHandler)
     {
     }
 
     /**
      * @Route(path="/new", name="article_new")
      */
-    public function newAction()
+    public function newAction(ViewArticleHandler $articleHandler, Request $request)
     {
-        // Seul les auteurs doivent avoir access.
+        if(!$this->getUser()->IsAuthor())
+            throw new AccessDeniedHttpException();
+
+        $article = new Article();
+        $em = $this->getDoctrine()->getManager();
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $article->setAuthor($this->getUser());
+            $slug = new Slug\SlugGenerator();
+            $article->setSlug($slug->generate($article->getTitle()));
+            $article->setCreatedAt(new DateTime('now'));
+            $article->setUpdatedAt(new DateTime('now'));
+
+            $em->persist($article);
+            $em->flush();
+        }
+
+        return $this->render("Article/new.html.twig", ["form"=>$form->createView()]);
+
     }
 
     /**
      * @Route(path="/update/{slug}", name="article_update")
      */
-    public function updateAction()
+    public function updateAction(ViewArticleHandler $articleHandler)
     {
         // Seul les auteurs doivent avoir access.
         // Seul l'auteur de l'article peut le modifier
